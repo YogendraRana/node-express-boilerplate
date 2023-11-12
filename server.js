b7d712cf03360bc64fbbfa2e96390207c5e047b8
Yogendra Rana <yogendrarana4321@gmail.com>
2023-11-12 21:36:21 +0545

    Minor changes

--- server.js
+++ server.js
@@ -2,11 +2,9 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
 import cookieParser from 'cookie-parser';
+import { connectdb } from './config/database.js';
 import { corsOptions } from './config/corsOptions.js';
 import { credentials } from './middlewares/credentialsMiddleware.js';
-
-// imports
-import { connectdb } from './config/database.js';
 
 // import middleware
 import ErrorMiddleware from './middlewares/errorMiddleware.js';
@@ -15,36 +13,31 @@ import ErrorMiddleware from './middlewares/errorMiddleware.js';
 import authRoutes from './routes/authRoutes.js'
 import adminRoutes from './routes/adminRoutes.js'
 
-
 // dov env
 dotenv.config({path: '.env'});
 
-
 // express app
 const app = express();
+
+// middlewares
+app.use(credentials)
 app.use(express.json());
 app.use(cookieParser());
-app.use(credentials)
 app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }));
-
 
 // configuration
 connectdb();
 
-
 // port
 const PORT = process.env.PORT || 8000;
-
 
 // listen
 app.listen(PORT, () => console.log(`Listening on port number ${PORT}`));
 
-
 // routes
 app.use("/api/v1", authRoutes)
 app.use("/api/v1", adminRoutes)
-
 
 // error middleware
 app.use(ErrorMiddleware);
